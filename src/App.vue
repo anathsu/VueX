85783c9c55cbabce3f312dd37fcbff69c1d5bae0
anathsu <willian.silva@al.infnet.edu.br>
2020-11-19 12:53:59 -0300

    Add Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <b-row>
+      teste
+    </b-row>
     <router-view/>
   </div>
 </template>
